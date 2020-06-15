61de70f0ed7c5b7e9ce9fdea3869b1aeee393d00
joedillonuk <joe.dillon.uk@gmail.com>
2020-06-15 16:18:16 +0100

    Changed to-do list to objects. Added HIGH PRIORITY text indicator. Added Radio Buttons for priority and made form fields required.

--- src/app.js
+++ src/app.js
@@ -4,13 +4,25 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: "#app",
     data: {
-      items: ["Buy Lasagne", "Cook Lasagne", "Eat Lasagne"],
-      newItem: ""
+      items: [
+        {name: "Buy Lasagne", highPriority: true},
+        {name: "Cook Lasagne", highPriority: true},
+        {name: "Eat Lasagne", highPriority: false}
+      ],
+      newItem: "",
+      priority: ""
     },
     methods: {
 
       saveNewItem: function() {
-        this.items.push(this.newItem)
+        let tempPriority = false
+        if(this.priority === "true"){
+          tempPriority = true
+        }
+        this.items.push({
+          name: this.newItem,
+          highPriority: tempPriority
+        })
         this.newItem = "";
       }
 
